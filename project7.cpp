00eaff48764b85a9e92028574184c483988ca6dc
Sam Berkson <sberkson@zagmail.gonzaga.edu>
2021-03-01 16:51:13 -0800

    project7.cpp

--- project7.cpp
+++ project7.cpp
@@ -1,3 +1,10 @@
+/*
+Name: Sam Berkson
+Class: CPSC 122-01
+Date Submitted: 3/01/2021
+Assignment: Project 7
+Description: Program encrypts/decrypts text file using a transposition cipher.  Uses a 2d array to hold the key, switching columns and sorting to change between encryption and decryption. Takes in input from inFile and writes to outFile for encrypt, takes in input from outFile and writes to inFile for decryption mode.
+*/
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -17,7 +24,7 @@ void closeFiles(fstream&, fstream&, fstream&);
 int main(int argc, char* argv[]) 
 {
   int mode = atoi(argv[1]);
-
+  //Mode selector
   if(mode == 0)
   {
     keyGen(argv[2]); 
@@ -28,7 +35,7 @@ int main(int argc, char* argv[])
   }
   return 0;
 }
-
+//Generates key
 void keyGen(string keyFileName)
 {
   fstream keyFile;
@@ -37,7 +44,7 @@ void keyGen(string keyFileName)
   bool validate[26] = {false};
 
   fileOpen(keyFile, keyFileName, 'w');
-
+  //Finds number that hasnt been found between 0-25 and adds to key[]
   while(pos < 26)
   {
     while(true)
@@ -53,13 +60,13 @@ void keyGen(string keyFileName)
       }
     }
   }
-  
+  //Writes key to keyFile
   for(int i = 0; i < 26; i++)
   {
     keyFile << i << " " << key[i] << endl;
   }
 }
-
+//Encrypt/Decrypt function
 char transform(char ch, int encDec[26][2])
 {
   if(ch == ' ')
@@ -81,13 +88,13 @@ void fileControl(string keyFileName, string inFileName, string outFileName, int 
   fstream keyFile, inFile, outFile;
   string input;
   int encDec[26][2];
-
+  //Encrypt mode
   if(mode == 1)
   {
     fileOpen(keyFile, keyFileName, 'r');
     fileOpen(inFile, inFileName, 'r');
     fileOpen(outFile, outFileName, 'w');
-    
+    //Grabs key
     for(int i = 0; i < 26; i++)
     {
       keyFile >> encDec[i][0];
@@ -95,7 +102,7 @@ void fileControl(string keyFileName, string inFileName, string outFileName, int 
       keyFile >> encDec[i][1];
       getline(keyFile, input);
     }
-
+    //Grabs input from inFile using string i/o and encrypts, then writes to outFile
     while(inFile.peek() != EOF)
     {
       getline(inFile, input); 
@@ -109,14 +116,15 @@ void fileControl(string keyFileName, string inFileName, string outFileName, int 
     }
   
   }
+  //Decrypt mode
   if(mode == 2)
   {
-    int tempMatrix[26][2];
+    int tempMatrix[26][2]; //Used to switch sides of array
 
     fileOpen(keyFile, keyFileName, 'r');
     fileOpen(inFile, inFileName, 'w');
     fileOpen(outFile, outFileName, 'r');
-
+    //Grabs key
     for(int i = 0; i < 26; i++)
     {
       keyFile >> encDec[i][0];
@@ -124,7 +132,7 @@ void fileControl(string keyFileName, string inFileName, string outFileName, int 
       keyFile >> encDec[i][1];
       getline(keyFile, input);
     }
-
+    //Fills temp matrix (encDec but flipped columns)
     for(int i = 0; i < 26; i++)
     {
       int temp = 1;
@@ -134,9 +142,9 @@ void fileControl(string keyFileName, string inFileName, string outFileName, int 
         temp--;
       }  
     }
-
+    //Sorts the flipped encDec matrix
     sortyBoi(tempMatrix);
-
+    //Grabs input from outFile using string i/o, then decrypts and writes back to inFile
     while(outFile.peek() != EOF)
     {
       getline(outFile, input); 
@@ -149,9 +157,10 @@ void fileControl(string keyFileName, string inFileName, string outFileName, int 
       inFile << endl;
     }     
   }
+  //Closes files
   closeFiles(keyFile, inFile, outFile);
 }
-
+//Opens files
 void fileOpen(fstream& file, string name, char mode)
 {
   string fileType;
@@ -179,8 +188,10 @@ void fileOpen(fstream& file, string name, char mode)
     exit(EXIT_FAILURE);
   }
 }
+//Sorting function
 void sortyBoi(int (&tempMatrix)[26][2])
 {
+  //Loops through 2d array, sorting the first column and bringing the 2nd column along for the ride
   for(int i = 0; i < 26; i++)
     {
       bool swap = false;
@@ -198,12 +209,13 @@ void sortyBoi(int (&tempMatrix)[26][2])
           swap = true;
         }
       }
-      if(!swap)
+      if(!swap) //If no swap, break and repeat loop
       {
         break;
       }
     }
 }
+//Closes files
 void closeFiles(fstream& keyFile, fstream& inFile, fstream& outFile)
 {
   if(keyFile.is_open())
